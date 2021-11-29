160df680fba7c54cbb04327946d59f8f78139eb8
Kameron Ashley <kameronashley@gmail.com>
2021-11-28 21:28:30 -0800

    commit tree 2d428cddf4eebe955b645e276a5c3c97423b6fe0

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,23 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else 
+      items = Item.all 
+    end
     render json: items, include: :user
   end
 
+  def show
+    item = Item.find_by(id: params[:id])
+    render json: item, include: :user
+  end
+
+  def create
+    # user = User.find(params[:user_id])
+    new_user_item = Item.create(id: params[:id], user_id: params[:user_id], name: params[:name], description: params[:description], price: params[:price])
+    render json: new_user_item, include: :user
+  end
 end
